8ec99a615ec183399bfb9e5a6b3d60d6eba53643
Luis Carrion <lcarrionalvarez@gmail.com>
2021-08-17 20:04:26 -0500

    Asynchronous requests with redux implemented

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login } from '../../actions/auth';
+import { startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
@@ -19,7 +19,7 @@ export const LoginScreen = () => {
 
   const handleLogin = (e) => {
     e.preventDefault()
-    dispatch( login(email, password) )
+    dispatch( startLoginEmailPassword(email, password) )
   }
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
 import { authReducer } from '../reducers/authReducer';
 
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 
 const reducers = combineReducers ({
@@ -9,6 +13,8 @@ const reducers = combineReducers ({
 
 export const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers( 
+    applyMiddleware( thunk )
+  )
 )
 
