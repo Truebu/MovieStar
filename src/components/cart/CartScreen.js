307059febb016f5bb83fcd167c88d97ed34c699f
Luis Carrion <lcarrionalvarez@gmail.com>
2021-10-04 16:19:10 -0500

    Libs For Boostrap Removed

--- src/components/cart/CartScreen.js
+++ src/components/cart/CartScreen.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+
+import { MovieCartItem } from './MovieCartItem'
 
 export const CartScreen = () => {
+
+  const {cart} = useSelector(state => state.cart)
+
   return (
     <Container>
       <Row>
@@ -14,13 +20,25 @@ export const CartScreen = () => {
       <Row>
         <Col>Back</Col>
         <Col>Your Movies</Col>
-        <Col>Buy All</Col>        
-      </Row>      
+        <Col>Buy All</Col>
+      </Row>
       <Row>
         <Container>
           <Row>
             <Col>fdsa</Col>
             <Col>fdsafds</Col>
+          </Row>
+          <Row>
+            <div>
+              {
+                cart?.map(movie => (
+                  <MovieCartItem
+                    key={movie.id}
+                    {...movie}
+                  />
+                ))
+              }              
+            </div>
           </Row>
         </Container>
       </Row>
